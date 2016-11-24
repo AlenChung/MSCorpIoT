080e731a902d7e13f85cc11d9992d566672098e7
AlenChung <Allen.Chung@msseed.idv.tw>
2016-11-24 15:30:13 +0800

    Taiwan Event

--- MSCorp.FirstResponse.Client App+Source/MSCorp.FirstResponseDemo/src/MSCorp.FirstResponse.Client/Configuration.cs
+++ MSCorp.FirstResponse.Client App+Source/MSCorp.FirstResponseDemo/src/MSCorp.FirstResponse.Client/Configuration.cs
@@ -8,13 +8,13 @@ namespace MSCorp.FirstResponse.Client
     public static class Configuration
     {
         public const string MapKey = "IpjMhSrzSwXU1U21gEwr~Z71YXlz9Txfh28UIfKz49A~AuevJ7P1M0aOBmypIoOA8cWHygeIENrGXB9dc0yQkUgm2NLKUumG8-o1VOUGxPXD";
-        public const double ZoomLevel = 14.1;
+        public const double ZoomLevel = 16.1;    //If you want to Change your map zoom size
         public const double IncidentZoomLevel = 16;
 
-        public static readonly Geopoint StartLocation = new Geopoint(new BasicGeoposition { Latitude = 47.597893, Longitude = -122.01724 });
+        public static readonly Geopoint StartLocation = new Geopoint(new BasicGeoposition { Latitude = 25.081182, Longitude = 121.586708 });
         public static readonly TimeSpan UpdateInterval = TimeSpan.FromMilliseconds(80);
         public static readonly BasicGeoposition[] PolygonSearch = {
-            new BasicGeoposition { Latitude = 47.584540, Longitude = -122.022430 },
+            new BasicGeoposition { Latitude = 25.032282, Longitude = 121.568276 },
             new BasicGeoposition { Latitude = 47.580847, Longitude = -122.022217 },
             new BasicGeoposition { Latitude = 47.577210, Longitude = -122.014425 },
             new BasicGeoposition { Latitude = 47.577954, Longitude = -122.003700 },
@@ -33,16 +33,16 @@ namespace MSCorp.FirstResponse.Client
         public const double MovementSpeed = 0.0001; //0.00004;
         public const int MaxIncidentsInProgress = 15;
 
-        public const double UserLatitude = 47.588400;
-        public const double UserLongitude = -122.035594;
+        public const double UserLatitude = 25.041687;    //47.588400  This is my user point & user-car
+        public const double UserLongitude = 121.562737;  //-122.035594
 
-        public const double MapStartLatitude = 47.601631;
-        public const double MapStartLongitude = -122.062454;
+        public const double MapStartLatitude = 25.038182;    //Allen UI Start Point with FirstResponse MS-TW
+        public const double MapStartLongitude = 121.567362;
 
-        public const double AmbulanceLatitude = 47.575516;
-        public const double AmbulanceLongitude = -121.988506;
+        public const double AmbulanceLatitude = 25.040373;
+        public const double AmbulanceLongitude = 121.573194;
 
-        public const string IncidentJsonDataFile = "Data/IncidentData.json";
+        public const string IncidentJsonDataFile = "Data/IncidentData.json";   //Aleardy Change with Taiwan Event
         public const string ResponderJsonDataFile = "Data/ResponderData.json";
         public const string TicketJsonDataFile = "Data/TicketData.json";
         public const string ResponderRoutesJsonDataFile = "Data/ResponderRoutes.json";
